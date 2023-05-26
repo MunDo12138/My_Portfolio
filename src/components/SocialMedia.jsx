0cd30b8ff95fe853004030b4a50108e5a81f600c
MunDo12138 <66548936+MunDo12138@users.noreply.github.com>
2023-05-26 11:33:57 +0800

    Update Social Media component

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { BsGithub, BsLinkedin } from 'react-icons/bs'
-import { FaFacebookF } from 'react-icons/fa'
+import { BsGithub, BsLinkedin, BsPersonFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-          <BsLinkedin />  
+          <Link to="https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:83ba7da6-d281-39c9-b5ee-9652119a6900" target="_blank" rel="noopener noreferrer">
+            <BsPersonFill />
+          </Link>
         </div>
         <div>
-          <BsGithub />
+          <Link to="https://www.linkedin.com/in/hungleongli/" target="_blank" rel="noopener noreferrer">
+            <BsLinkedin />
+          </Link>
+        </div>
+        <div>
+          <Link to="https://github.com/MunDo12138" target="_blank" rel="noopener noreferrer">
+            <BsGithub />
+          </Link>
         </div>
     </div>
   )
