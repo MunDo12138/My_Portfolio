fe7260e425c66f59dc153c7b1f3d17fe6139eaa0
MunDo12138 <66548936+MunDo12138@users.noreply.github.com>
2023-06-17 17:53:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+import { inject } from '@vercel/analytics';
+
 const root = createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <App />
     </BrowserRouter>
 );
+
+inject();
